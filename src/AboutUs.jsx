2b445b93a05761df3eeac11060e102defd4e1006
atharva-0085 <agangrad@uncc.edu>
2026-09-23 09:38:47 -0400

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <section className="about-us">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery, your destination for beautiful and
+        healthy houseplants. We believe plants can transform any space into
+        a more relaxing, welcoming, and vibrant environment.
+      </p>
+
+      <p>
+        At Paradise Nursery, we offer a wide variety of houseplants for
+        different spaces and lifestyles. Whether you are looking for
+        air-purifying plants, low-maintenance plants, or decorative plants,
+        we have something for you.
+      </p>
+
+      <p>
+        Our goal is to make it easy for everyone to bring nature into their
+        home and enjoy the benefits of indoor plants.
+      </p>
+    </section>
+  );
+}
+
+export default AboutUs;
